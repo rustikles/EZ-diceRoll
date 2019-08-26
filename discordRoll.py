2c7e9368d7f4592d90bbb2415560b96ced506be6
rustikles <rustikles@users.noreply.github.com>
2019-08-25 20:11:37 -0700

    Create discordRoll.py

---
+++ discordRoll.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import random
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def roll(ctx, number):
+  try:
+    arg = random.randint(1, int(number))
+  except ValueError:
+    await ctx.send("What the fuck is that???")
+  else:
+    await ctx.send(str(arg))
+    
+bot.run(TOKEN)
